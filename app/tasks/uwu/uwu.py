48141728d73b033b561d634e52738d36cccf51d6
Randall Crock <rlcrock@gmail.com>
2021-11-18 07:16:27 -0700

    Additional functions in greetings

--- app/tasks/uwu/uwu.py
+++ app/tasks/uwu/uwu.py
@@ -8,6 +8,9 @@ _LOG = logging.getLogger('discord-util').getChild("uwu")
 class Uwu(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.content.lower().startswith('hello bot'):
+        if message.content.lower().startswith('hello bot') or message.content.lower().startswith('hewwo bot'):
             await message.channel.send('Hewwo uwu')
             return
+
+        if message.content.lower().startswith('good bot'):
+            await message.add_reaction("\N{FLUSHED FACE}")
